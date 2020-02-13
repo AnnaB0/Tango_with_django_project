c6c3251987a4b6f64413959a9c1801cdf10239dd
Anna Berry <2373329b@student.gla.ac.uk>
2020-02-13 15:53:12 +0000

    chapter 6

--- media/media/urls.py
+++ media/media/urls.py
@@ -1,6 +1,4 @@
-#C:\Users\Anna\Workspace\tango_with_django_project\tango_with_django_project\urls.py
-
-"""tango_with_django_project URL Configuration
+"""media URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -17,14 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.urls import include
-from rango import views
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',views.index, name='index'),
-    path('rango/',include('rango.urls')),
-    
     path('admin/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
